ea3aedb7725ee91bb68a6ba014d260e9393a8b3f
Bridgette Kennedy <bridgetteakennedy@gmail.com>
2019-10-10 21:12:25 -0400

    Master to master branch

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config['DEBUG']= True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:lc101@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
+app.secret_key = 'ShhhhhItsasecret'
 
 db = SQLAlchemy(app)
 
@@ -21,17 +22,16 @@ class Blog(db.Model):
         self.body = body
 
 @app.route('/', methods=['POST', 'GET'])
-@app.route('/blog', methods=['POST', 'GET'])
+# @app.route('/blog', methods=['POST', 'GET'])
 def index():
-
+    # blog_id = Blog.query.filter_by(id=id).first()
     posts = Blog.query.all()
     
-    if request.method=='GET' and Blog.id !=0:
-        return render_template('blog.html', posts=posts, page_title="Home", ids=id)
-    elif request.method=='GET' and Blog.id ==0:
-        return "<h1>I'm empty blog id</h1>"
+    if id == 6:
+        pass
     else:
-        return "<h1>I'm a post</h1>"
+        return render_template('blog.html', posts=posts, page_title="Home", ids=id)
+    
 
 @app.route('/blog/<string:id>', methods=['POST', 'GET'])
 def individ(id):
@@ -50,10 +50,27 @@ def add():
         new_title = request.form['title']
         new_body = request.form['body']
         new_post = Blog(new_title, new_body)
-        db.session.add(new_post)
-        db.session.commit()
+
+        if new_title == "":
+            new_body = request.form['body']
+            flash("Please enter a title")
+            return render_template('newpost.html', page_title="Add a New Entry", post=new_body)
+
+        if new_body == "":
+            new_title = request.form['title']
+            flash("Please enter a body for the message")
+            return render_template('newpost.html', page_title="Add a New Entry", title=new_title)   
+        if new_title != "" and new_body != "":
+            db.session.add(new_post)
+            db.session.commit()
+        
         new_id = new_post
-        return render_template('individ_entry.html', posts=posts, new_id=new_id)
+        id=new_id.id
+        redirection = "/blog/"+str(id)
+        return redirect(redirection)
+        # paramater = "/blog?id=" + str(new_id.id)
+        # # return render_template('individ_entry.html', new_id=new_id)
+        # return redirect (paramater)
 
         if new_id != "Null":
             return render_template('entry.html', posts=posts, id=new_id)
